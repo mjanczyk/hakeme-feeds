aeae50997bc641aa6a9a39dd51e04c9c6537b75b
Marcin Janczyk <marcinjanczyk@gmail.com>
2013-07-13 21:20:36 +0000

    Simple & synchronous in-memory feed cache
    ...this should be thread-safe, I hope? No idea.

--- src/Main.hs
+++ src/Main.hs
@@ -9,11 +9,16 @@ import Yesod.AtomFeed (atomFeed)
 import Yesod.RssFeed (rssFeed)
 import Yesod.Feed
 import Text.Blaze (toMarkup, preEscapedToMarkup)
-import Data.Time.Clock (getCurrentTime)
+import Data.Time.Clock (UTCTime, getCurrentTime, addUTCTime)
+import Data.Time.Format (readTime)
+import System.Locale (defaultTimeLocale)
+import Control.Concurrent.MVar
 
 import Scrapper (fetchPage, pageToPosts, Post (..), )
 
-data HakemeFeeds = HakemeFeeds
+data HakemeFeeds = HakemeFeeds {
+  feedCache :: MVar (UTCTime, Feed (Route HakemeFeeds))
+}
 
 data HLang = Pl | En deriving (Show, Eq, Read)
 data Format = Atom | RSS deriving (Show, Eq, Read)
@@ -63,15 +68,32 @@ getExternalR url = undefined
 urlByLang Pl = T.pack "http://hakeme.pl"
 urlByLang En = T.pack "http://hakeme.com"
 
-getAtomFeedR lang = atomFeed =<< liftIO (feed lang Atom)
-getRssFeedR lang = rssFeed =<< liftIO (feed lang RSS)
+cacheDurationSecs = 60 * 60 * 4 -- 4 hours
+
+retrieveFeed lang format = do
+  yesod <- getYesod
+  now <- liftIO getCurrentTime
+  let cacheMVar = feedCache yesod
+  liftIO $ modifyMVar cacheMVar (\(oldTime, oldFeed) ->
+    if (addUTCTime cacheDurationSecs oldTime) < now
+      then do -- replace cache
+        putStrLn "fetching site"
+        feed <- feed lang format
+        return ((now, feed), feed)
+      else do
+        putStrLn "using cache"
+        return ((oldTime, oldFeed), oldFeed))
+
+getAtomFeedR lang = atomFeed =<< retrieveFeed lang Atom
+getRssFeedR lang = rssFeed =<< retrieveFeed lang RSS
 
 feed lang format = do
   let url = urlByLang lang
   page <- fetchPage url
   now <- getCurrentTime
   let posts = pageToPosts url page
-  return $ feedFromPosts lang format url now posts
+  let feed = feedFromPosts lang format url now posts
+  return feed
 
 feedFromPosts lang format url time posts = do
   let entries = map postToEntry posts
@@ -92,7 +114,9 @@ postToEntry (Post { title = title, date = date, link = link, content = content }
             , feedEntryTitle = title
             , feedEntryContent = preEscapedToMarkup content }
 
-main = warpEnv HakemeFeeds
+main = do
+  cache <- newMVar (readTime defaultTimeLocale "%Y" "1970", undefined)
+  warpEnv HakemeFeeds { feedCache = cache }
 
 {-
 main :: IO ()
