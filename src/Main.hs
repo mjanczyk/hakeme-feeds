092ee6b10cd3e3f35c336e01c8ed57dcc8872969
Marcin Janczyk <marcinjanczyk@gmail.com>
2013-07-13 16:46:50 +0000

    Use StringLike and Text everywhere, instead of String

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,14 @@
 
 module Main where
 
-import Scrapper (page, parseAll, Post (..), )
+import Scrapper (page, pageToPosts, Post (..), )
 import qualified Data.Text as T
 
-url = "http://hakeme.pl"
+url = T.pack "http://hakeme.pl"
 
 -- | The main entry point.
 main :: IO ()
 main = do
-  p <- page url
-  mapM_ ((>> putStrLn "\n") . putStrLn . show ) $ parseAll url p
+  html <- page url
+  mapM_ ((>> putStrLn "\n") . putStrLn . show ) $ pageToPosts url html
   
--- src/Scrapper.hs
+++ src/Scrapper.hs
@@ -1,8 +1,9 @@
 module Scrapper where
 
 import Network.HTTP (simpleHTTP, getRequest, getResponseBody)
-import Text.HTML.TagSoup (Tag (TagOpen, TagClose), parseTags, sections, partitions, (~==), (~/=), innerText, isTagClose, renderTags, fromAttrib)
+import Text.HTML.TagSoup (Tag (TagOpen, TagClose), TagRep, parseTags, sections, partitions, (~==), (~/=), innerText, isTagClose, renderTags, fromAttrib)
 import Text.StringLike (StringLike)
+import qualified Text.StringLike as SL
 import Control.Monad (liftM)
 import qualified Data.Text as T
 import Data.Text (Text)
@@ -13,6 +14,9 @@ data Post = Post { title :: Text
                  , content :: Text }
          deriving (Show)
 
+s :: (StringLike str) => String -> str
+s = SL.fromString
+
 postTag = TagOpen "div" [("class", "post")]
 postTitleTag = TagOpen "h2" []
 postFooterTag = TagOpen "div" [("class", "post-footer")]
@@ -21,17 +25,22 @@ postCommentsTag = TagOpen "span" [("class", "post-comments")]
 postContentTag = TagOpen "div" [("class", "post-content")]
 linkTag = TagOpen "a" []
 
-mapLinksToHost :: String -> [Tag String] -> [Tag String]
+mapLinksToHost :: StringLike str => str -> [Tag str] -> [Tag str]
 mapLinksToHost host = map f
   where
-    f (TagOpen "a" attrs) = TagOpen "a" $ rep "href" (host++) attrs
-    f (TagOpen "img" attrs) = TagOpen "img" $ rep "src" (host++) attrs
+    -- f :: StringLike str => Tag str -> Tag str
+    f (TagOpen a attrs)   | a == s"a"     = TagOpen (s"a") $ rep (s"href") (SL.append host) attrs
+    f (TagOpen img attrs) | img == s"img" = TagOpen (s"img") $ rep (s"src") (SL.append host) attrs
     f tag = tag
+    -- rep :: StringLike str => str -> (str -> str) -> [(str, str)] -> [(str, str)]
     rep key tr [] = []
     rep key tr ((key',val):xs) | key == key' = (key, tr val):xs
     rep key tr (x:xs) = x:(rep key tr xs)
 
+takeUntilTagCloseAny :: StringLike str => [Tag str] -> [Tag str]
 takeUntilTagCloseAny = takeWhile $ not . isTagClose
+
+takeUntilTagClose :: StringLike str => str -> [Tag str] -> [Tag str]
 takeUntilTagClose tag = takeWhile $ (~/= (TagClose tag))
 
 posts :: StringLike str => [Tag str] -> [[Tag str]]
@@ -43,20 +52,20 @@ findTitle = innerText . takeUntilTagCloseAny . head . sections (~== postTitleTag
 findDate :: StringLike str => [Tag str] -> str
 findDate = innerText . takeUntilTagCloseAny . head . sections (~== postDateTag)
 
-findContent :: String -> [Tag String] -> String
-findContent host = renderTags . mapLinksToHost host . takeUntilTagClose "div" . head . sections (~== postContentTag)
+findContent :: StringLike str => str -> [Tag str] -> str
+findContent host = renderTags . mapLinksToHost host . takeUntilTagClose (s"div") . head . sections (~== postContentTag)
 
-findLink :: String -> [Tag String] -> String
-findLink host = (host++) . fromAttrib "href" . head . head . sections (~== linkTag) . head . sections (~== postTitleTag)
+findLink :: (StringLike str, Show str, Eq str) => str -> [Tag str] -> str
+findLink host = (SL.append host) . fromAttrib (SL.fromString "href") . head . head . sections (~== linkTag) . head . sections (~== postTitleTag)
 
-page :: String -> IO String
-page host = simpleHTTP (getRequest host) >>= getResponseBody
+page :: Text -> IO Text
+page host = simpleHTTP (getRequest $ T.unpack host) >>= getResponseBody >>= return . T.pack
 
-mkPost :: String -> [Tag String] -> Post
-mkPost host post = Post { title = T.strip $ T.pack $ findTitle post
-                   , date = T.strip $ T.pack $ findDate post
-                   , link = T.pack $ findLink host post
-                   , content = T.strip $ T.pack $ findContent host post }
+mkPost :: Text -> [Tag Text] -> Post
+mkPost host post = Post { title = T.strip $ findTitle post
+                   , date = T.strip $ findDate post
+                   , link = findLink host post
+                   , content = T.strip $ findContent host post }
 
-parseAll :: String -> String -> [Post]
-parseAll host page = map (mkPost host) $ posts $ parseTags page
+pageToPosts :: Text -> Text -> [Post]
+pageToPosts host page = map (mkPost host) $ posts $ parseTags page
